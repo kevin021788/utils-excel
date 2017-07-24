c094a968b6d4319c9b3081b4b877b780409fdaaa
李铭昕 <715557344@qq.com>
2017-07-24 17:20:19 +0800

    * 搭建单元测试结构

---
+++ tests/Excel/BaseTest.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | BaseTest.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\Utils;
+
+use limx\Utils\Excel;
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase
+{
+    public function testBaseCase()
+    {
+        echo 1;
+    }
+}
